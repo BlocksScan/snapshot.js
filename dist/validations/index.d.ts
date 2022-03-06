22502974ad1fab4601649989e0401b9d88861a9e
BlocksScanIO <support@blocksscan.io>
2022-03-06 11:23:53 +0400

    remove dist.

--- dist/validations/index.d.ts
+++
@@ -1,9 +0,0 @@
-import basic from './basic';
-import aaveSpaceValidation from './aave';
-import nounsSpaceValidation from './nouns';
-declare const _default: {
-    basic: typeof basic;
-    aave: typeof aaveSpaceValidation;
-    nouns: typeof nounsSpaceValidation;
-};
-export default _default;
